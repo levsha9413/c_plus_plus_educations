3adcc3ae85c127fdbe29f3a50e6107030d10d9c7
Бочкарев Павел <pbochkarev@ussc.ru>
2023-04-04 15:39:51 +0500

    Дописал методы, выделил заголовочный файл ЛР6

--- lr6.cpp
+++ lr6.cpp
@@ -1,72 +1,31 @@
 //
-// Created by pbochkarev on 29.03.2023.
+// Created by pbochkarev on 04.04.2023.
 //
-#include <iostream>
-#include <stdlib.h>
-
-using namespace std;
-
-class Text{
-
-private:
-    char *str;
-    int length;
-public:
-    Text(){
-      length = 0;
-      str = NULL;
-    }
-    Text(char message[]){
-        int counter = 0;
-        char* p = message;
-        while(*p){
-            ++p;
-            ++counter;
-        }
-        str = (char*)malloc(counter);
-        char *pointer = message;
-        int index = 0;
-        while (*pointer){
-            str[index++]= message[index];
-            ++pointer;
-        }
-        length = index;
-
-    }
-    Text(const Text& text){
-    }
-    ~Text(){
-        free(str);
-    }
-    Text& operator=(const Text& t){
-        length = t.getLength();
-        str = t.getText(); //не работает
-    }
-    Text& operator+(const Text&);
-    Text& operator==(const Text&);
-
-     int getLength()const{
-        return length;
-    }
-    char* getText() const{
-
-        return str;
-    }
-    void input(){
-        cout << "Введите текст:\n";
-        cin >> str;
-    }
-    void output(){
-        cout << "Содержимое объекта:\n";
-        cout << str << "\n";
-    }
-
-};
+#include "Text.h"
 
 int main(){
-    Text x = Text("abc");
-    char* s = x.getText();
-    Text a;
-    a = x;
-    a.output();
+    Text oldText = Text("abc");
+    Text newText;
+    newText = oldText;
+    cout << "\nРезультат операции = \n";
+    cout << "newText\n";
+    newText.output();
+    cout << "oldText\n";
+    oldText.output();
+    newText = newText + oldText;
+
+    cout << "\nРезультат операции + \n";
+    cout << "newText\n";
+    newText.output();
+    cout << "oldText\n";
+    oldText.output();
+
+    cout << "\nРезультат операции == \n";
+    if(newText == oldText){
+        cout << "Объекты равны\n";
+    }else{
+        cout << "Объекты НЕ равны\n";
+
+    }
+
 }
