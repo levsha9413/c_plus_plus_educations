f3e0d2f41e15ffa15f41148d4693cc780c3deb86
Бочкарев Павел <pbochkarev@ussc.ru>
2023-04-04 12:42:50 +0500

    Написал часть методов

--- lr6.cpp
+++ lr6.cpp
@@ -1,21 +1,72 @@
 //
 // Created by pbochkarev on 29.03.2023.
 //
+#include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+
 class Text{
 
 private:
-    char str[256];
+    char *str;
+    int length;
 public:
-    Text(char*);
     Text(){
+      length = 0;
+      str = NULL;
+    }
+    Text(char message[]){
+        int counter = 0;
+        char* p = message;
+        while(*p){
+            ++p;
+            ++counter;
+        }
+        str = (char*)malloc(counter);
+        char *pointer = message;
+        int index = 0;
+        while (*pointer){
+            str[index++]= message[index];
+            ++pointer;
+        }
+        length = index;
 
     }
-    Text(const Text&);
-    Text& operator=(const Text&);
+    Text(const Text& text){
+    }
+    ~Text(){
+        free(str);
+    }
+    Text& operator=(const Text& t){
+        length = t.getLength();
+        str = t.getText(); //не работает
+    }
     Text& operator+(const Text&);
     Text& operator==(const Text&);
-    int length();
-    void input();
-    void output();
+
+     int getLength()const{
+        return length;
+    }
+    char* getText() const{
+
+        return str;
+    }
+    void input(){
+        cout << "Введите текст:\n";
+        cin >> str;
+    }
+    void output(){
+        cout << "Содержимое объекта:\n";
+        cout << str << "\n";
+    }
 
 };
+
+int main(){
+    Text x = Text("abc");
+    char* s = x.getText();
+    Text a;
+    a = x;
+    a.output();
+}
